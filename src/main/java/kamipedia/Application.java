1a68c15a04381ebcc827456428712b43e335203a
YasuhiroKimesawa <kimesawa_yasuhiro@aratana.jp>
2017-03-19 22:53:39 +0900

    initail commit

---
+++ src/main/java/kamipedia/Application.java
@@ -0,0 +1,30 @@
+package kamipedia;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public ObjectMapper directFieldAccess()
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable( SerializationFeature.INDENT_OUTPUT);
+        mapper.setVisibility( PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY );
+        mapper.setVisibility( PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE );
+        mapper.setVisibility( PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE );
+        return mapper;
+    }
+}
